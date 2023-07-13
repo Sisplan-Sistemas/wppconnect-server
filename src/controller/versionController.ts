664709a615af5e6690b2352535cf5d6f2740340f
bruno-sisplan <bruno.sena@sisplansistemas.com.br>
2023-07-13 14:12:56 -0300

    criado nova rota pra pegar versão do whatsapp

--- src/controller/versionController.ts
+++ src/controller/versionController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
 
 export async function returnVersion(req: Request, res: Response) {
-  return res.status(200).json({ versao: '2.2.4-4' });
+  return res.status(201).json({ response: '2.2.4' });
 }
